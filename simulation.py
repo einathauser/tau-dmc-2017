d9ca8065ece5c98736e32a0dbd5f471280506467
Einat Hauser <ehauzer11@gmail.com>
2017-09-07 12:05:06 +0300

    with histogram- wasn't tested!

--- simulation.py
+++ simulation.py
@@ -1,14 +1,14 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
-
+import matplotlib.mlab as mlab
 # # function for counting particles in buckets:
 
 # we get a vector where the valueof x is the index of box it belongs to
 # These are the values of the particles in the matrix. to use the function i should write:
 # bucketing(item) for item in x_0.(values=x_0)
 def bucketing(x_min, x_max, values, n_boxes):
-    bucket_size = (x_max - x_min) / n_boxes
+    bucket_size = float(float(x_max - x_min) / float(n_boxes))
     last_bucket = n_boxes - 1
     matrix_buckets = np.zeros(n_boxes)  # [0] * n_boxes
     for value in values:
@@ -22,6 +22,18 @@ def bucketing(x_min, x_max, values, n_boxes):
 
     # if we have the range 0-12, and a bucket size of 4(three buckets:0,1,2), then 12 will fall
     # at bucket 3 index of bucket 2. To fix that, round just the maximal value back to the previous bucket.
+def matrix_location(x_min, x_max, n_boxes, values):
+    bucket_size = float(float(x_max - x_min) / float(n_boxes))
+    last_bucket = n_boxes - 1
+    indices = []
+    for value in values:
+        index = int((value - x_min) / bucket_size)
+        if index > last_bucket:
+           index = last_bucket
+        else:
+            index = index
+        indices.append(index)
+    return indices
 
 
 def count(matrix_buckets):
@@ -48,7 +60,7 @@ def particle_locations(x, dt):
     return location
 
 def energy(v_x, n_pre, n_0, dt):
-    return np.average(v_x) + (1 - (n_0 / n_pre)) / dt
+    return np.average(v_x) + (1.0 - float(n_pre) / n_0) / dt
     # sum_e_r += e_r
     # avg_e_r = sum_e_r / time_interval
     # if (previous * 100 / avg_e_r) <= 5:
@@ -59,34 +71,47 @@ def run_dmc(dt, n_times, n_0, x_min, x_max, n_boxes):
     V_x = V(x)
     e_r = np.average(V_x)
     e_rs = [e_r]
+    new_psy = 0
     for i in range(n_times):
+        print i
         # creates a vector of number with step dt. i gives the items in the list
         x = particle_locations(x, dt)
-        n_previous = len(x)
         V_x = V(x)
         W_x = W(V_x, e_r, dt)
         m_x = m(W_x)
         x = np.repeat(x, m_x)
+        n_previous = len(x)
+        # print('Round %d m_x: %s' % (i, np.mean(m_x)))
         e_r = energy(V_x, n_previous, n_0, dt)
         previous_avg = np.average(e_rs)
         e_rs.append(e_r)
-        new_psy = 0
         psy_s = []
-        if i >= 50:
+        buckets = []
+        if len(x) > 30 * 1000 * 1000:
+            raise Exception('Basa')
+        if i >= 50 & i < n_times:
             avg_e_r = np.average(e_rs[49:])
             standard_dev = np.std(e_rs[49:])
             num_buckets = bucketing(x_min, x_max, x, n_boxes)
+            buckets += matrix_location(x_min, x_max, x, n_boxes)
             psy = new_psy
             new_psy = count(num_buckets)
-            current_psy = psy *(i-49) + new_psy/(2+i-49)
-            psy_s.append(current_psy)
-    return "delta E_r: ", standard_dev, "E_r: ", avg_e_r
+    mu, sigma = ((x_max + x_min)/2), 5)
+    x_0 = mu + sigma * np.random.randn(n_boxes)
+
+    # the histogram of the data
+    n, bins, patches = plt.hist(x_0, n_boxes, normed=1, facecolor='green', alpha=0.75)
+    plt.axis([x_min, x_max, 0, 1.0])
+    plt.grid(True)
+    plt.show()
+
+    return standard_dev, avg_e_r
+
+
 
             # if (psy_s[i-51] * 100 / psy) <= 1 & (previous_avg * 100 /avg_e_r) <= 1  :
 
 
-    plt.plot(e_rs)
-    plt.show()
 #animation of psy
 
 
@@ -103,13 +128,13 @@ def run_dmc(dt, n_times, n_0, x_min, x_max, n_boxes):
 
 
 
-if __name__ == "__main__":
+# if __name__ == "__main__":
     # execute only if run as a script
     # count(np.random.uniform(0, 1999, 10000), 2000)
-    n_0 = 5000
-    x_min = -2
-    x_max = 2
-    n_boxes = 2000
+    n_0 = 500
+    x_min = -2.0
+    x_max = 2.0
+    n_boxes = 200
     dt = 0.1
-    n_times = 50
-    run_dmc(dt, n_times, n_0, x_min, x_max, n_boxes)
+    n_times = 2000
+print run_dmc(dt = 0.1, n_times = 2000, n_0 = 500, x_min = -2.0, x_max = 2.0, n_boxes = 200)
